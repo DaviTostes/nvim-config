79c33544aae0359c395b7138a3cc69b6c3cad36b
davitostes <davisiqueira591@gmail.com>
2024-02-26 14:26:17 -0300

    auto-session

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,9 +2,20 @@ return {
   'akinsho/toggleterm.nvim',
   config = function()
     require("toggleterm").setup({
-      vim.keymap.set('n', 'TT', ":ToggleTerm direction=float name=desktop<CR>", {}),
+      vim.keymap.set('n', 'TT', ":ToggleTerm direction=float<CR>", {}),
       vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], {}),
       vim.keymap.set('n', 'TS', ":TermSelect<CR>", {}),
+      float_opts = {
+        title_pos = "center"
+      },
+      vim.keymap.set("n", "<C-t>", function()
+                local new_term = require("toggleterm.terminal").Terminal:new({
+                    direction = "float", -- Ou "horizontal" se preferir
+                })
+                new_term:toggle()
+            end, { noremap = true, silent = true }),
+
+            -- Outros mapeamentos e configurações aqui...
     })
   end
 }
